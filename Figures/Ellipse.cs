23fafce9b3a8cd28de870230c140ff9a30e151b1
Ilya Kosykh <ilyuha.prosto.22041961@gmail.com>
2021-04-06 12:44:59 +0300

    s

--- Figures/Ellipse.cs
+++ Figures/Ellipse.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Threading.Tasks;
-using System.Windows.Shapes;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace Figures
 {
